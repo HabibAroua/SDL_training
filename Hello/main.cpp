4e1e2b68c13e2daa7d96489002b75a41b4bf207a
HabibAroua <habibha.aroua82@gmail.com>
2018-02-07 21:35:22 +0100

    edit size of window

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -6,7 +6,8 @@ int main ( int argc, char** argv )
     SDL_Init(SDL_INIT_VIDEO);
     //Create the window
     SDL_Surface *fenetre=NULL;
-    fenetre=SDL_SetVideoMode(500 , 500, 32 , SDL_HWSURFACE);
+    fenetre=SDL_SetVideoMode(1000 , 1000, 32 , SDL_HWSURFACE);
+    //Boucle
     while(1)
     {
          SDL_Event event;
@@ -16,6 +17,7 @@ int main ( int argc, char** argv )
               break;
          }
     }
+    //Free memorie
     SDL_FreeSurface(fenetre);
 	//Quitter SDL
 	SDL_Quit();
