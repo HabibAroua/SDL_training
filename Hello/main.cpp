b3ebddf1d2b2e891c1207b95b17b9d60334534b1
HabibAroua <habibha.aroua82@gmail.com>
2018-02-07 21:24:17 +0100

    Show the window

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -1,16 +1,17 @@
 #include <SDL/SDL.h>
 #include<stdio.h>
+#include <SDL.h>
 int main ( int argc, char** argv )
 {
-    if (SDL_Init( SDL_INIT_EVERYTHING ) !=0)
+    SDL_Init(SDL_INIT_VIDEO);
+    //Create the window
+    SDL_Surface *fenetre=NULL;
+    fenetre=SDL_SetVideoMode(500 , 500, 32 , SDL_HWSURFACE);
+    while(1)
     {
-        printf("Error %s \n",SDL_GetError());
-    }
-    else
-    {
-        printf("Succesful \n");
-    }
 
+    }
+    SDL_FreeSurface(fenetre);
 	//Quitter SDL
 	SDL_Quit();
     return 0;
