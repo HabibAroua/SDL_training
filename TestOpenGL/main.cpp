d0824440de082920bd02f19b9497874d4531bcf4
HabibAroua <habibha.aroua82@gmail.com>
2018-02-21 15:35:10 +0100

    open gl

---
+++ TestOpenGL/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glx.h>    /* this includes the necessary X headers */
+#include <GL/gl.h>
+
+#include <X11/X.h>    /* X11 constant (e.g. TrueColor) */
+#include <X11/keysym.h>
+using namespace std;
+
+static int snglBuf[] = {GLX_RGBA, GLX_DEPTH_SIZE, 16, None};
+static int dblBuf[]  = {GLX_RGBA, GLX_DEPTH_SIZE, 16, GLX_DOUBLEBUFFER, None};
+
+Display   *dpy;
+Window     win;
+GLfloat    xAngle = 42.0, yAngle = 82.0, zAngle = 112.0;
+GLboolean  doubleBuffer = GL_TRUE;
+
+void fatalError(char *message)
+{
+  fprintf(stderr, "main: %s\n", message);
+  exit(1);
+}
+
+void redraw(void)
+{
+  static GLboolean   displayListInited = GL_FALSE;
+
+  if (displayListInited)
+  {
+    /* if display list already exists, just execute it */
+//    glCallList(1);
+  }
+  else
+  {
+    /* otherwise compile and execute to create the display list */
+    glNewList(1, GL_COMPILE_AND_EXECUTE);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* front face */
+    glBegin(GL_QUADS);
+      glColor3f(0.0, 0.7, 0.1);  /* green */
+      glVertex3f(-1.0, 1.0, 1.0);
+      glVertex3f(1.0, 1.0, 1.0);
+      glVertex3f(1.0, -1.0, 1.0);
+      glVertex3f(-1.0, -1.0, 1.0);
+
+      /* back face */
+      glColor3f(0.9, 1.0, 0.0);  /* yellow */
+      glVertex3f(-1.0, 1.0, -1.0);
+      glVertex3f(1.0, 1.0, -1.0);
+      glVertex3f(1.0, -1.0, -1.0);
+      glVertex3f(-1.0, -1.0, -1.0);
+
+      /* top side face */
+      glColor3f(0.2, 0.2, 1.0);  /* blue */
+      glVertex3f(-1.0, 1.0, 1.0);
+      glVertex3f(1.0, 1.0, 1.0);
+      glVertex3f(1.0, 1.0, -1.0);
+      glVertex3f(-1.0, 1.0, -1.0);
+
+      /* bottom side face */
+      glColor3f(0.7, 0.0, 0.1);  /* red */
+      glVertex3f(-1.0, -1.0, 1.0);
+      glVertex3f(1.0, -1.0, 1.0);
+      glVertex3f(1.0, -1.0, -1.0);
+      glVertex3f(-1.0, -1.0, -1.0);
+    glEnd();
+    glEndList();
+    displayListInited = GL_TRUE;
+  }
+  if (doubleBuffer)
+    glXSwapBuffers(dpy, win);/* buffer swap does implicit glFlush */
+  else
+    glFlush();  /* explicit flush for single buffered case */
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ openGL/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+#include <GL/gl.h>			/* OpenGL header file */
+#include <GL/glu.h>			/* OpenGL utilities header file */
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+    cout<<"Hello world "<<endl;
+    return 0;
+}
